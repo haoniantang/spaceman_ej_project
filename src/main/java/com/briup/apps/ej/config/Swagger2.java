9d647e8cd00930d3ed57bdd85f3e28efd655a907
haotangnian <15649566594@163.com>
2019-06-10 13:50:29 +0800

    项目初始化

--- src/main/java/com/briup/apps/ej/config/Swagger2.java
+++ src/main/java/com/briup/apps/ej/config/Swagger2.java
@@ -38,7 +38,7 @@ public class Swagger2 {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.apiInfo(apiInfo())
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.briup.apps.app02.web"))
+				.apis(RequestHandlerSelectors.basePackage("com.briup.apps.ej.web"))
 				.paths(PathSelectors.any())
 				.build();
 	}
